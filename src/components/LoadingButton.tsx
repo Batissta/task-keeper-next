73980aa2ae12eaf347f107a2a28497101d7cf0a6
Batissta <francinaldofilho05@gmail.com>
2025-03-28 20:57:57 -0300

    feat: creating a loadingButton component

---
+++ src/components/LoadingButton.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { Button } from "./ui/button";
+import { CirclePlus } from "lucide-react";
+import { Loader2 } from "lucide-react";
+import { MouseEventHandler } from "react";
+
+const LoadingButton = ({
+  loading,
+  desabilitado,
+  onClick,
+}: {
+  loading: boolean;
+  desabilitado: boolean;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+}) => {
+  return (
+    <>
+      {!loading && (
+        <Button
+          disabled={desabilitado}
+          variant="outline"
+          className="cursor-pointer hover:bg-transparent w-[80%]"
+          onClick={onClick}
+        >
+          <CirclePlus />
+          Cadastrar Task
+        </Button>
+      )}
+      {loading && (
+        <Button
+          disabled
+          className="cursor-pointer hover:bg-transparent w-[80%]"
+          onClick={onClick}
+        >
+          <Loader2 className="animate-spin" />
+          Please wait
+        </Button>
+      )}
+    </>
+  );
+};
+
+export default LoadingButton;
