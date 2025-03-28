d1d3387e0617b8bb80c68cee3fcd51198e489bbd
Batissta <francinaldofilho05@gmail.com>
2025-03-28 20:58:17 -0300

    feat: creating a TaskCard component

---
+++ src/components/TaskCard.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Card, CardTitle } from "./ui/card";
+import { Asterisk, Check, MoveUpRight } from "lucide-react";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+
+const TaskCard = ({
+  title,
+  finished,
+}: {
+  title: string;
+  finished: boolean;
+}) => {
+  return (
+    <Card className="font-(family-name:--font-poppins) p-6 bg-zinc-950 border-zinc-600 shadow-2xs shadow-zinc-600">
+      <div className="justify-between items-center flex gap-6">
+        <CardTitle className="text-xl font-[500] text-zinc-300 col-span-3">
+          {title}
+        </CardTitle>
+        {finished && (
+          <span className="bg-green-600 rounded-[50%] p-1">
+            <Check className="text-zinc-300" />
+          </span>
+        )}
+        {!finished && (
+          <span className="bg-blue-700 rounded-[50%] p-1">
+            <Asterisk className="text-zinc-300" />
+          </span>
+        )}
+      </div>
+      <Separator className="bg-zinc-700" />
+      <div>
+        <p className="text-zinc-500">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto
+          tempore commodi reiciendis, sapiente, neque aliquid facilis deleniti
+          dolorem eaque veritatis vero molestiae aspernatur, deserunt nam. Nihil
+          nulla atque distinctio assumenda!
+        </p>
+        <Button className="mt-4 text-zinc-300 group transition w-fit duration-200 cursor-pointer overflow-hidden">
+          <p className="translate-x-[-200%] group-hover:translate-x-0 duration-600 opacity-0 hidden group-hover:opacity-100 group-hover:to-100% group-hover:block ease-in">
+            Ver a tarefa
+          </p>
+          <MoveUpRight />
+        </Button>
+      </div>
+    </Card>
+  );
+};
+
+export default TaskCard;
