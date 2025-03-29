e9590bf19e45fd275531ca4c99aa9c77605245fb
Batissta <francinaldofilho05@gmail.com>
2025-03-29 10:10:38 -0300

    styles: taskCard

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -6,23 +6,24 @@ import { Button } from "./ui/button";
 
 const TaskCard = ({
   title,
+  description,
   finished,
 }: {
   title: string;
+  description: string;
   finished: boolean;
 }) => {
   return (
-    <Card className="font-(family-name:--font-poppins) p-6 bg-zinc-950 border-zinc-600 shadow-2xs shadow-zinc-600">
+    <Card className="min-w-[400px] font-(family-name:--font-poppins) p-6 bg-zinc-950 border-zinc-600 shadow-2xs shadow-zinc-600">
       <div className="justify-between items-center flex gap-6">
         <CardTitle className="text-xl font-[500] text-zinc-300 col-span-3">
           {title}
         </CardTitle>
-        {finished && (
+        {finished ? (
           <span className="bg-green-600 rounded-[50%] p-1">
             <Check className="text-zinc-300" />
           </span>
-        )}
-        {!finished && (
+        ) : (
           <span className="bg-blue-700 rounded-[50%] p-1">
             <Asterisk className="text-zinc-300" />
           </span>
@@ -31,14 +32,13 @@ const TaskCard = ({
       <Separator className="bg-zinc-700" />
       <div>
         <p className="text-zinc-500">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto
-          tempore commodi reiciendis, sapiente, neque aliquid facilis deleniti
-          dolorem eaque veritatis vero molestiae aspernatur, deserunt nam. Nihil
-          nulla atque distinctio assumenda!
+          {description
+            ? `${description.slice(0, 120)}...`
+            : "Vou criar um componente aqui para dizer que ainda não tem uma descrição para essa atividade"}
         </p>
-        <Button className="mt-4 text-zinc-300 group transition w-fit duration-200 cursor-pointer overflow-hidden">
+        <Button className="mt-4 text-zinc-300 group transition w-fit duration-200 cursor-pointer overflow-hidden bg-zinc-800 hover:bg-zinc-700">
           <p className="translate-x-[-200%] group-hover:translate-x-0 duration-600 opacity-0 hidden group-hover:opacity-100 group-hover:to-100% group-hover:block ease-in">
-            Ver a tarefa
+            Editar atividade
           </p>
           <MoveUpRight />
         </Button>
