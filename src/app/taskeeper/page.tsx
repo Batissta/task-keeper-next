f81a94d80a0de00a78598237aef452b8e32d63e5
Batissta <francinaldofilho05@gmail.com>
2025-03-29 10:08:37 -0300

    refactor: improving the content at the taskeeper page

--- src/app/taskeeper/page.tsx
+++ src/app/taskeeper/page.tsx
@@ -1,17 +1,28 @@
-import { listaTasks } from "@/serverActions/apiTask";
-import React from "react";
+import Loading from "@/components/Loading";
+import TasksList from "@/components/TasksList";
+import { MoveRight } from "lucide-react";
+import Link from "next/link";
+import React, { Suspense } from "react";
 
-const Taskeeper = async () => {
-  const data = await listaTasks();
+const Taskeeper = () => {
   return (
-    <main>
-      <h1>Suas Tarefas ficam por aqui.</h1>
-      {data.sucess &&
-        data.tasks.map((task, i) => (
-          <div key={`${task.title}_${i}`}>
-            <p>{task.title}</p>
-          </div>
-        ))}
+    <main className="bg-zinc-800 min-h-screen font-(family-name:--font-poppins) px-16 py-12">
+      <div className="flex flex-col">
+        <h1 className="text-zinc-300 text-4xl font-black tracking-tight">
+          Suas Tarefas ficam por aqui.
+        </h1>
+        <Link
+          href={"/"}
+          className="text-zinc-400 flex gap-2 hover:text-zinc-500 text-[1rem] mt-2"
+        >
+          Organize suas ideias, viagens, projetos ou tarefas! <MoveRight />
+        </Link>
+      </div>
+      <div className="mt-8">
+        <Suspense fallback={<Loading />}>
+          <TasksList />
+        </Suspense>
+      </div>
     </main>
   );
 };
