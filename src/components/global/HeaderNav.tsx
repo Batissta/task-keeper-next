f883c7519a78a9ef08ddcf3788d710ce020d0f27
Batissta <francinaldofilho05@gmail.com>
2025-03-30 12:05:04 -0300

    feat: responsive

--- src/components/global/HeaderNav.tsx
+++ src/components/global/HeaderNav.tsx
@@ -1,7 +1,9 @@
 "use client";
-import { BadgePlus, Files, UserSearch } from "lucide-react";
+import { BadgePlus, Files, Fingerprint } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import useMedia from "@/hooks/useMedia";
+import HeaderNavMobile from "./HeaderNavMobile";
 
 enum ePaths {
   home = "/",
@@ -10,39 +12,42 @@ enum ePaths {
 }
 
 const HeaderNav = () => {
-  return (
-    <nav>
-      <ul className="flex gap-4 items-center text-zinc-600 uppercase">
-        <Link
-          href={ePaths.home}
-          className={`bg-zinc-800 rounded-2xl hover:bg-transparent md:bg-transparent group px-4 py-2 md:hover:bg-zinc-800 hover:rounded-2xl duration-150`}
-        >
-          <li className="flex gap-2 group-hover:text-zinc-500 duration-150 text-[.875rem] items-center">
-            <BadgePlus />
-            Cadastre
-          </li>
-        </Link>
-        <Link
-          href={ePaths.taskeeper}
-          className={`bg-zinc-800 rounded-2xl hover:bg-transparent md:bg-transparent group px-4 py-2 md:hover:bg-zinc-800 hover:rounded-2xl duration-150`}
-        >
-          <li className="flex gap-2 group-hover:text-zinc-500 duration-150 text-[.875rem] items-center">
-            <Files />
-            Seus arquivos
-          </li>
-        </Link>
-        <Link
-          href={ePaths.conheca}
-          className={`bg-zinc-800 rounded-2xl hover:bg-transparent md:bg-transparent group px-4 py-2 md:hover:bg-zinc-800 hover:rounded-2xl duration-150`}
-        >
-          <li className="flex gap-2 group-hover:text-zinc-500 duration-150 text-[.875rem] items-center">
-            <UserSearch />
-            Conheça o autor
-          </li>
-        </Link>
-      </ul>
-    </nav>
-  );
+  const mobile = useMedia("(width <= 800px)");
+  if (!mobile)
+    return (
+      <nav>
+        <ul className="flex gap-4 items-center text-zinc-200 uppercase">
+          <Link
+            href={ePaths.home}
+            className={`rounded-2xl group px-4 py-2 hover:bg-zinc-800 hover:rounded-2xl duration-150`}
+          >
+            <li className="flex gap-2 group-hover:text-zinc-400 duration-150 text-[.875rem] items-center">
+              <BadgePlus />
+              Cadastre
+            </li>
+          </Link>
+          <Link
+            href={ePaths.taskeeper}
+            className={`rounded-2xl group px-4 py-2 hover:bg-zinc-800 hover:rounded-2xl duration-150`}
+          >
+            <li className="flex gap-2 group-hover:text-zinc-400 duration-150 text-[.875rem] items-center">
+              <Files />
+              Seus arquivos
+            </li>
+          </Link>
+          <Link
+            href={ePaths.conheca}
+            className={`rounded-2xl group px-4 py-2 hover:bg-zinc-800 hover:rounded-2xl duration-150`}
+          >
+            <li className="flex gap-2 group-hover:text-zinc-400 duration-150 text-[.875rem] items-center">
+              <Fingerprint />
+              Conheça o autor
+            </li>
+          </Link>
+        </ul>
+      </nav>
+    );
+  return <HeaderNavMobile />;
 };
 
 export default HeaderNav;
