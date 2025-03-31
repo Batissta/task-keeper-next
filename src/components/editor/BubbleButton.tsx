023b597b70fed36d93c11490b0decb30a2fad928
Batissta <francinaldofilho05@gmail.com>
2025-03-31 17:01:54 -0300

    feat: bubbleButtons working

--- src/components/editor/BubbleButton.tsx
+++ src/components/editor/BubbleButton.tsx
@@ -7,7 +7,7 @@ export interface BubbleButtonProps extends ComponentProps<"button"> {
 const BubbleButton = (props: BubbleButtonProps) => {
   return (
     <button
-      className="p-3 cursor-pointer text-zinc-200 text-sm flex items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600"
+      className="p-3 cursor-pointer text-zinc-200 text-sm flex justify-between items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600 data-[active=true]:text-violet-400"
       {...props}
     />
   );
